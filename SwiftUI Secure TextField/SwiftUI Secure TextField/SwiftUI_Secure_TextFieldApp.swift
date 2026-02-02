25b23479c970255189f2745f8fc2b0bc84a284aa
Bhargav Joshi <joshibhargav110@gmail.com>
2026-02-02 15:28:03 +0530

    Project Setup

---
+++ SwiftUI Secure TextField/SwiftUI Secure TextField/SwiftUI_Secure_TextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Secure_TextFieldApp.swift
+//  SwiftUI Secure TextField
+//
+//  Created by Bhargav Joshi on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Secure_TextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
